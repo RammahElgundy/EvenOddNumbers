2159b762f13a52c3d172d805639c542fb5760082
Elgundy <rammahelgundy@gmail.com>
2025-02-17 21:41:24 +0200

    initial commit

---
+++ EvenOddNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+PrintNumbers("All Numbers", numbers);
+PrintNumbers("Even Numbers", numbers.Where(IsEven));
+PrintNumbers("Odd Numbers", numbers.Where(IsOdd));
+
+Console.WriteLine("Hello, World!");
+
+static void PrintNumbers(string title, IEnumerable<int> numbers)
+{
+    Console.Write($"{title}: [");
+	foreach (var n in numbers)
+	{
+        Console.Write($" {n}");
+	}
+    Console.WriteLine($" ]");
+}
+
+
+
+static bool IsEven(int number)
+{
+    if(number %2 == 0)
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+static bool IsOdd(int number)
+{
+    if (number % 2 != 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
